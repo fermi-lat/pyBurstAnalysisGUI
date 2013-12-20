4b176c09f81c6257d0e13e34af4345425c037c77
giacomov <>
2013-12-20 00:06:41 +0000

    ops...

--- python/GtBurst/getDataPath.py
+++ python/GtBurst/getDataPath.py
@@ -7,6 +7,6 @@ def getDataPath():
   dataPath             = os.path.join(installationPath,'data')
   if(not os.path.exists(os.path.join(dataPath,'glast_logo.png'))):
     #In the version within the Science Tools, data are saved in data/GtBurst/
-    dataPath           = os.path.join(dataPath,'GtBurst')
+    dataPath           = os.path.join(dataPath,'pyBurstAnalysisGUI')
   pass
   return dataPath
