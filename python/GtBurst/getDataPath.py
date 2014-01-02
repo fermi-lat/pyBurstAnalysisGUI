dc1ea520519193348386881161aebb068e1b68b8
giacomov <>
2014-01-02 22:12:31 +0000

    Now the data path is /home/giacomov/Glast/ScienceTools-09-32-05/refdata/fermi in the case of Fermi public science tools

--- python/GtBurst/getDataPath.py
+++ python/GtBurst/getDataPath.py
@@ -4,9 +4,22 @@ import os
 def getDataPath():
   path                 = GtBurst.__file__
   installationPath     = os.path.join(os.path.sep.join(path.split(os.path.sep)[0:-3]))
+  
+  #Stand-alone version: data are in the 'data' subdir under the installationPath
   dataPath             = os.path.join(installationPath,'data')
+  
   if(not os.path.exists(os.path.join(dataPath,'glast_logo.png'))):
-    #In the version within the Science Tools, data are saved in data/GtBurst/
-    dataPath           = os.path.join(dataPath,'pyBurstAnalysisGUI')
+    #In the SCONS version of Science Tools, data are saved in data/pyBurstAnalysisGUI/
+    dataPath           = os.path.join(installationPath,'data','pyBurstAnalysisGUI')
+    if(not os.path.exists(os.path.join(dataPath,'glast_logo.png'))):
+      #In the public version of Fermi ST, data are in refdata/fermi/pyBurstAnalysisGUI
+      dataPath         = os.path.join(installationPath,'refdata','fermi','pyBurstAnalysisGUI')
+    pass
   pass
+  
+  #Final check
+  if(not os.path.exists(os.path.join(dataPath,'glast_logo.png'))):
+    raise RuntimeError("Fatal error: could not locate the data subdirectory.")
+  pass
+  
   return dataPath
