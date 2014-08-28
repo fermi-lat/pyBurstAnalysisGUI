ce0f744869347badda54aab99889921867ed4316
giacomov <>
2014-08-28 21:16:09 +0000

    Synch with git

--- python/GtBurst/IRFS.py
+++ python/GtBurst/IRFS.py
@@ -120,20 +120,12 @@ IRFS['P8_TRANSIENT_R020_V1'] = IRF('P8_TRANSIENT_R020_V1','P8_TRANSIENT_R020_V1'
 IRFS['P8_SOURCE_V1']         = IRF('P8_SOURCE_V1','P8_SOURCE_V1','300',4,'gll_iem_v05.fits','p8_p300x_isotropic_source_v1.txt')                
 
 #P8        
-<<<<<<< HEAD
 IRFS['P8_TRANSIENT_R100'] = IRF('P8_TRANSIENT_R100','P8_TRANSIENT_R100_V4','301',4,'template_4years_P8_V2_scaled.fits','isotropic_source_4years_P8V3.txt')
 IRFS['P8_TRANSIENT_R020'] = IRF('P8_TRANSIENT_R020','P8_TRANSIENT_R020_V4','301',16,'template_4years_P8_V2_scaled.fits','isotropic_source_4years_P8V3.txt')
 IRFS['P8_TRANSIENT_R010'] = IRF('P8_TRANSIENT_R010','P8_TRANSIENT_R010_V4','301',64,'template_4years_P8_V2_scaled.fits','isotropic_source_4years_P8V3.txt')
 IRFS['P8_SOURCE']         = IRF('P8_SOURCE','P8_SOURCE_V4','301',128,'template_4years_P8_V2_scaled.fits','isotropic_source_4years_P8V3.txt')                
 IRFS['P8_CLEAN']          = IRF('P8_CLEAN','P8_CLEAN_V4','301',256,'template_4years_P8_V2_scaled.fits','isotropic_clean_4years_P8V3.txt')                
 IRFS['P8_ULTRACLEAN']     = IRF('P8_ULTRACLEAN','P8_ULTRACLEAN_V4','301',512,'template_4years_P8_V2_scaled.fits','isotropic_ultraclean_4years_P8V3.txt')                
-=======
-IRFS['P8_TRANSIENT_R100'] = IRF('P8_TRANSIENT_R100','P8_TRANSIENT_R100_V4','301',4,'gll_iem_v05.fits','p8_p300x_isotropic_source_v1.txt')
-IRFS['P8_TRANSIENT_R020'] = IRF('P8_TRANSIENT_R020','P8_TRANSIENT_R020_V4','301',16,'gll_iem_v05.fits','p8_p300x_isotropic_source_v1.txt')
-IRFS['P8_TRANSIENT_R010'] = IRF('P8_TRANSIENT_R010','P8_TRANSIENT_R010_V4','301',64,'gll_iem_v05.fits','p8_p300x_isotropic_source_v1.txt')
-IRFS['P8_SOURCE']         = IRF('P8_SOURCE','P8_SOURCE_V4','301',128,'gll_iem_v05.fits','p8_p300x_isotropic_source_v1.txt')                
-
->>>>>>> b9bc22999ef45289d21dcacb72cd5a6c1016c3bc
 
 PROCS                         = collections.OrderedDict()
 for k,v in IRFS.iteritems():
