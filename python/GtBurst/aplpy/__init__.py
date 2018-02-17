4224c00275730b6c57a15cadcac9a00505cc0b21
Giacomo Vianello <giacomo.vianello@gmail.com>
2018-02-16 16:26:15 -0800

    In Conda we want to use their aplpy

--- python/GtBurst/aplpy/__init__.py
+++ python/GtBurst/aplpy/__init__.py
@@ -1,1 +1,9 @@
-from aplpy import *
+try:
+    # Try and load the aplpy from the environment
+    from aplpy import *
+
+except ImportError:
+    
+    # Use internal aplpy
+    
+    from my_aplpy import *
